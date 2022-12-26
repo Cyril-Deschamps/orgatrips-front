ebbf30fe853fe8af6cb712c159d6b9b6c13e423c
Cyril Deschamps <contact@cyrildeschamps.fr>
2022-12-26 18:14:38 +0100

    feat: imported boilerplate

---
+++ src/footer/FooterCopyright.tsx
@@ -0,0 +1,21 @@
+import { AppConfig } from "../utils/AppConfig";
+
+const FooterCopyright = () => (
+  <div className={"footer-copyright"}>
+    © Copyright {new Date().getFullYear()} {AppConfig.title}. Cyril Deschamps
+
+    <style jsx>
+      {`
+        .footer-copyright :global(a) {
+          @apply text-primary-500;
+        }
+
+        .footer-copyright :global(a:hover) {
+          @apply underline;
+        }
+      `}
+    </style>
+  </div>
+);
+
+export { FooterCopyright };
---
+++ src/layout/Meta.tsx
@@ -0,0 +1,66 @@
+import { NextSeo } from "next-seo";
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+import { AppConfig } from "../utils/AppConfig";
+
+type IMetaProps = {
+  title: string;
+  description: string;
+  canonical?: string;
+};
+
+const Meta = (props: IMetaProps) => {
+  const router = useRouter();
+
+  return (
+    <>
+      <Head>
+        <meta key={"charset"} charSet={"UTF-8"} />
+        <meta
+          key={"viewport"}
+          content={"width=device-width,initial-scale=1"}
+          name={"viewport"}
+        />
+        <link
+          key={"apple"}
+          href={`${router.basePath}/apple-touch-icon.png`}
+          rel={"apple-touch-icon"}
+        />
+        <link
+          key={"icon32"}
+          href={`${router.basePath}/favicon-32x32.png`}
+          rel={"icon"}
+          sizes={"32x32"}
+          type={"image/png"}
+        />
+        <link
+          key={"icon16"}
+          href={`${router.basePath}/favicon-16x16.png`}
+          rel={"icon"}
+          sizes={"16x16"}
+          type={"image/png"}
+        />
+        <link
+          key={"favicon"}
+          href={`${router.basePath}/favicon.ico`}
+          rel={"icon"}
+        />
+      </Head>
+      <NextSeo
+        canonical={props.canonical}
+        description={props.description}
+        openGraph={{
+          title: props.title,
+          description: props.description,
+          url: props.canonical,
+          locale: AppConfig.locale,
+          site_name: AppConfig.site_name,
+        }}
+        title={props.title}
+      />
+    </>
+  );
+};
+
+export { Meta };
