d9f1125b7e5d42cce67493ce869d51d4c65d8767
Cyril Deschamps <contact@cyrildeschamps.fr>
2023-03-09 10:56:34 +0100

    fix: fixed ui mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,6 @@ import AppLayout from "../services/ui/Layout/AppLayout";
 import { default as NextLink } from "next/link";
 
 import dashboardIllustration from "../assets/img/dashboard-illustration.svg";
-import logoBookingClassic from "../assets/img/logo-booking-classic.svg";
 import logoKiwiClassic from "../assets/img/logo-kiwi-classic.svg";
 import sectionCutBackground from "../assets/img/section-cut-background.svg";
 import drawnArrow from "../assets/img/drawn-arrow.svg";
@@ -26,7 +25,7 @@ import BaseSeo from "../services/seo/BaseSeo";
 import { jsonLdScriptProps } from "react-schemaorg";
 import { Organization } from "schema-dts";
 import { hostBaseURL } from "../services/auth/config";
-import { BOOKING_LINK, KIWI_LINK } from "../routes/external";
+import { KIWI_LINK } from "../routes/external";
 
 const Home = (): JSX.Element => {
   const { t } = useTranslation(["destination", "home", "website"]);
@@ -48,29 +47,33 @@ const Home = (): JSX.Element => {
       <main className={"flex flex-col items-center"}>
         <SizedSection
           className={
-            "flex flex-row justify-between z-10 min-h-[23rem] items-start md:min-h-[26rem] xl:items-start md:justify-center lg:justify-between"
+            "flex flex-row z-10 min-h-[28rem] items-start sm:min-h-[26rem] justify-center lg:justify-between"
           }
         >
-          <div className={"ml-xs sm:ml-xl md:ml-0 lg:ml-2xl relative"}>
-            <h1
+          <div
+            className={
+              "ml-xs sm:ml-xl md:ml-0 lg:ml-2xl relative flex flex-col items-center gap-s"
+            }
+          >
+            <span
               className={
-                "md:w-[31rem] font-VarsityTeam text-xl sm:text-2xl md:text-4xl leading-8 md:leading-10 tracking-wide md:text-center lg:text-start"
+                "font-Roboto inline-block md:absolute right-[-2rem] top-[-2.5rem] p-xxs sm:py-0 bg-light-blue text-white rounded-md md:rotate-6 text-xs leading-none sm:leading-normal sm:text-base font-bold uppercase"
               }
             >
-              <span
-                className={
-                  "absolute right-[-3rem] top-[-2rem] md:top-[-3rem] px-xs md:px-xs bg-light-blue text-white rounded-md rotate-6 text-xl sm:text-2xl"
-                }
-              >
-                {t("home:coming_soon")}
-              </span>
+              {t("home:coming_soon")}
+            </span>
+            <h1
+              className={
+                "font-VarsityTeam md:w-full text-4xl md:text-5xl leading-8 md:leading-10 sm:tracking-wide text-center lg:text-start"
+              }
+            >
               <Trans
                 components={{
                   green: <span className={"text-green"} />,
                   little: (
                     <div
                       className={
-                        "sm:text-xl md:text-2xl leading-6 md:leading-8 w-[15rem] md:w-[22rem] text-base md:mx-auto lg:mx-0"
+                        "text-lg md:text-3xl leading-7 md:leading-8 w-[90%] md:w-[26rem] mx-auto lg:mx-0 tracking-normal pt-xs"
                       }
                     />
                   ),
@@ -79,11 +82,10 @@ const Home = (): JSX.Element => {
                 {t("home:main_title")}
               </Trans>
             </h1>
-
             <Image
               alt={"drawn-arrow"}
               className={
-                "absolute right-[-5rem] top-6 w-24 md:right-[-8rem] sm:w-32 md:w-36"
+                "hidden md:block absolute bottom-[-3rem] left-[33rem] w-28 md:w-36"
               }
               src={drawnArrow}
             />
@@ -99,7 +101,7 @@ const Home = (): JSX.Element => {
         </SizedSection>
         <div
           className={
-            "bg-white w-full mt-[-54px] flex flex-col items-center pb-12"
+            "bg-white w-full mt-[-54px] flex flex-col items-center pb-6 md:pb-12"
           }
         >
           <SizedSection
@@ -118,7 +120,7 @@ const Home = (): JSX.Element => {
             </p>
           </SizedSection>
           <SizedSection className={"pt-4xl"}>
-            <div className={"pl-[2.5vw]"}>
+            <div className={"sm:pl-[2.5vw]"}>
               <TitleLegend>{t("home:our_partners_legend")}</TitleLegend>
               <Title2>{t("home:our_partners_title")}</Title2>
               <Divider className={"w-64"} />
@@ -127,16 +129,6 @@ const Home = (): JSX.Element => {
                   "flex flex-row gap-x-3xl gap-y-s pt-xl flex-wrap w-[40%] items-center"
                 }
               >
-                <NextLink href={BOOKING_LINK}>
-                  <Image
-                    alt={"Logo Booking"}
-                    className={
-                      "max-h-l lg:max-h-2xl object-contain object-left grayscale"
-                    }
-                    src={logoBookingClassic}
-                  />
-                </NextLink>
-
                 <NextLink href={KIWI_LINK}>
                   <Image
                     alt={"Logo kiwi"}
@@ -146,7 +138,7 @@ const Home = (): JSX.Element => {
                 </NextLink>
               </div>
             </div>
-            <div className={"mt-[-7rem] lg:mt-[-6rem]"}>
+            <div className={"mt-[-5rem] lg:mt-[-6rem]"}>
               <Image
                 alt={"section-background relative"}
                 className={"w-full h-[26rem]"}
@@ -154,33 +146,41 @@ const Home = (): JSX.Element => {
               />
               <div
                 className={
-                  "mt-[-26rem] pt-5 md:pt-8 pb-8 px-l lg:px-10 gap-2xl flex flex-col"
+                  "mt-[-26rem] pt-5 md:pt-8 pb-8 md:px-l lg:px-10 gap-xl flex flex-col"
                 }
               >
                 <div className={"flex flex-row-reverse"}>
                   <div
                     className={
-                      "grid grid-cols-2 md:grid-cols-4 p-xs gap-l md:gap-[6.5vw] w-[45%] h-[7.5rem] row-auto items-center justify-items-center"
+                      "mx-xs md:mx-0 grid grid-cols-2 md:grid-cols-4 p-xs gap-xs md:gap-[6.5vw] w-[45%] h-[7.5rem] row-auto items-center justify-items-center"
                     }
                   >
                     <Image
                       alt={"icon-plane"}
-                      className={"max-w-[3rem] w-full h-full md:h-auto"}
+                      className={
+                        "max-w-[2.5rem] sm:max-w-[3.5rem] w-full h-full md:h-auto"
+                      }
                       src={planeIcon}
                     />
                     <Image
                       alt={"icon-train"}
-                      className={"max-w-[3.5rem] w-full h-full md:h-auto"}
+                      className={
+                        "max-w-[2.5rem] sm:max-w-[3.5rem] w-full h-full md:h-auto"
+                      }
                       src={trainIcon}
                     />
                     <Image
                       alt={"icon-hotel"}
-                      className={"max-w-[3.5rem] w-full h-full md:h-auto"}
+                      className={
+                        "max-w-[2.5rem] sm:max-w-[3.5rem] w-full h-full md:h-auto"
+                      }
                       src={hotelIcon}
                     />
                     <Image
                       alt={"icon-best-price"}
-                      className={"max-w-[3.5rem] w-full h-full md:h-auto"}
+                      className={
+                        "max-w-[2.5rem] sm:max-w-[3.5rem] w-full h-full md:h-auto"
+                      }
                       src={bestPriceIcon}
                     />
                   </div>
@@ -190,13 +190,16 @@ const Home = (): JSX.Element => {
                     "flex-1 min-h-0 flex flex-row items-center flex-wrap"
                   }
                 >
-                  <Image
-                    alt={"trip-image"}
-                    className={
-                      "w-full md:w-1/3 max-h-52 md:h-48 object-cover rounded-md"
-                    }
-                    src={tripImage}
-                  />
+                  <div className={"w-full md:w-1/3 px-m md:px-0"}>
+                    <Image
+                      alt={"trip-image"}
+                      className={
+                        "rounded-md object-cover h-[14rem] md:h-[12.5rem]"
+                      }
+                      src={tripImage}
+                    />
+                  </div>
+
                   <div
                     className={
                       "pt-3xl md:pt-0 md:pl-xl lg:pl-4xl w-full md:w-2/3"
