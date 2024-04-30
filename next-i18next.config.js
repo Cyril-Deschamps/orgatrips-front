2437ecfb877bdb2f37c2a037f15839d69ed6bfd5
Cyril Deschamps <contact@cyrildeschamps.fr>
2024-04-30 15:52:42 +0200

    fix: fixed deployment

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -16,13 +16,11 @@ module.exports = {
         return `./src/services/i18n/website/${locale}.json`;
       case "validations":
         return `./src/services/validations/i18n/${locale}.json`;
-      case "trip":
-        return `./src/services/trip/i18n/${locale}.json`;
       case "pages_content":
         return `./src/services/i18n/pages_content/${locale}.json`;
       default:
         return namespace !== "common" ?
-          `./src/services/i18n/pages/${namespace}/${locale}.json` : `./src/services/i18n/pages/website/${locale}.json`;
+          `./src/services/${namespace}/i18n/${locale}.json` : `./src/services/i18n/website/${locale}.json`;
     }
   },
   reloadOnPrerender: process.env.NODE_ENV === "development",
